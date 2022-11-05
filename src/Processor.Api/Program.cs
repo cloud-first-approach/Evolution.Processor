7d1fec6789c9f24c211f98ca7a13f27ccc09c6c4
Sourabh Rustagi <sourabh.rustagi@hotmail.com>
2022-11-06 01:37:49 +0530

    Add Helath check

--- src/Processor.Api/Program.cs
+++ src/Processor.Api/Program.cs
@@ -1,16 +1,10 @@
 
 using App.Metrics.AspNetCore;
 using App.Metrics.Formatters.Prometheus;
+using Processor.Api.Extensions;
+using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.SystemConsole.Themes;
-using Serilog;
-using Processor.Api;
-using Processor.Api.Middlewares;
-using Amazon.S3;
-using Amazon.S3.Transfer;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.AspNetCore.Http.Features;
-using Processor.Api.Services;
 
 Log.Logger = new LoggerConfiguration().MinimumLevel
                .Debug()
@@ -44,49 +38,8 @@ builder.Host.UseMetricsWebTracking()
                         };
                     }
                 );
-
-builder.Services.Configure<KestrelServerOptions>(options =>
-{
-    options.Limits.MaxRequestBodySize = Int32.MaxValue; // if don't set default value is: 30 MB
-});
-builder.Services.Configure<FormOptions>(options =>
-{
-    options.MemoryBufferThreshold = Int32.MaxValue;
-    options.MultipartBoundaryLengthLimit = Int32.MaxValue;
-    options.MultipartBodyLengthLimit = Int32.MaxValue;
-    options.MultipartHeadersLengthLimit = Int32.MaxValue;
-});
-builder.Services.AddJwtAuthentication(builder.Configuration);
-builder.Services.AddControllers().AddDapr();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddHttpClient();
-builder.Services.AddScoped<IStorageService, StorageService>();
-
-builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
-builder.Services.AddAWSService<IAmazonS3>();
-builder.Services.AddTransient<TransferUtility>();
-
+builder.BuildServices();
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-//app.UseJwtParser();
-
-app.UseCloudEvents();
-app.MapControllers();
-app.MapSubscribeHandler();
-
-
+app.BuildApp();
 
 app.Run();
